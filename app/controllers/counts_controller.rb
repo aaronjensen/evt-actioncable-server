699ecd5c20c8eaa47f698382cd9e39b796e3e7be
Aaron Jensen <aaronjensen@gmail.com>
2020-04-11 12:44:56 -0700

    Counts controller

---
+++ app/controllers/counts_controller.rb
@@ -0,0 +1,18 @@
+require "count_component/controls"
+
+class CountsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def index
+    render json: { data: [
+      { id: 1, count: 2 },
+      { id: 2, count: 3 },
+    ] }
+  end
+
+  def update
+    count_id = params[:id]
+    CountComponent::Controls::Write::Increment.(count_id: count_id)
+    head :ok
+  end
+end
